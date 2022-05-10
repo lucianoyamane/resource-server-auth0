92ab30616becce34e1e17e7d3c817ee0bf244dd2
lyamanek <luciano.oliveira@klab.com.br>
2022-05-10 13:52:49 -0300

    init commit

---
+++ src/main/java/br/com/example/resourceserverauth0/ResourceServerAuth0Application.java
@@ -0,0 +1,15 @@
+package br.com.example.resourceserverauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerAuth0Application.class, args);
+	}
+
+}
+
+
---
+++ src/main/java/br/com/example/resourceserverauth0/model/Message.java
@@ -0,0 +1,14 @@
+package br.com.example.resourceserverauth0.model;
+
+public class Message {
+
+    private final String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
